231c596230c123f5bc18d70071fe654a9e24380d
최도은 <choidoeun0102@gmail.com>
2025-03-06 11:30:12 +0900

    헤더 내용 및 스타일 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ const Header = () => {
           <a href="#profile" className="hover:text-gray-400">PROFILE</a>
         </li>
         <li>
-          <a href="#projects" className="hover:text-gray-400">PROJECTS</a>
+          <a href="#contact" className="hover:text-gray-400">SKILLS</a>
         </li>
         <li>
-          <a href="#contact" className="hover:text-gray-400">CONTACT</a>
+          <a href="#projects" className="hover:text-gray-400">PROJECTS</a>
         </li>
       </ul>
     </header>
